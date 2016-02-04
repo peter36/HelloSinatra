515585ed5450414dfad37794e1f1cb2852e621bf
Peter Li <peter0306@gmail.com>
2016-02-04 10:17:34 -0800

    update configuration

--- hello.rb
+++ hello.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
+# configurations
+# to allow connection from another computer
+set :bind, '0.0.0.0'
+# to override local port
+set :port, 3000
+
+# response to '/hello' url
 get '/hello' do
   "Hello, World!"
 end
