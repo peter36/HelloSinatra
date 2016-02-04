74ae3dc77a7c5bf0a5dd2b176968abf870e77f30
Peter Li <peter0306@gmail.com>
2016-02-04 11:21:42 -0800

    Add route to'/'

--- hello.rb
+++ hello.rb
@@ -6,6 +6,11 @@ set :bind, '0.0.0.0'
 # to override local port
 set :port, 3000
 
+# response to '/' url
+get '/' do
+  "This is the index page"
+end
+
 # response to '/hello' url
 get '/hello' do
   "Hello, World!"
